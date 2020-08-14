4e1f5dc3f24e205fa67eecc80f9424ec0508e931
AjayGowda7 <ajaylgowda99@gmail.com>
2020-08-14 11:13:26 +0530

    Updates

---
+++ precache-manifest.9b57a67291ca2e90de9bede4ec3b7252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5444bdefcc27fafb260963b0792f1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe7af7446fa788999c07",
+    "url": "/static/css/main.898c017f.chunk.css"
+  },
+  {
+    "revision": "f6683532279832adc15f",
+    "url": "/static/js/2.a8ee570a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a8ee570a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7af7446fa788999c07",
+    "url": "/static/js/main.07686de5.chunk.js"
+  },
+  {
+    "revision": "afef85084f0804340619",
+    "url": "/static/js/runtime-main.2b370e64.js"
+  }
+]);
